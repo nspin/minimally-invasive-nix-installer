ce2041cfb2fd39cf176d11c7b1cf12e57d134bb0
Nick Spinale <nick@nickspinale.com>
2021-10-07 16:00:50 +0000

    add aarch64 support

---
+++ dist/install-x86_64-linux.sh
@@ -0,0 +1,88 @@
+{ # Prevent execution if this script was only partially downloaded
+
+set -Eeuo pipefail
+
+dest=/nix
+tarball_url=https://github.com/nspin/minimally-invasive-nix-installer/raw/dist-dlp5yb3c19/dist/min-nix-2.4pre20210802_47e96bb-x86_64-linux.tar.gz
+tarball_sha256=007d74de69d5a9e4ff21849cc622e04707faa56852350a2e33bc7fd66e5df6eb
+archive_name=min-nix-2.4pre20210802_47e96bb-x86_64-linux
+env_store_path=/nix/store/2gry0arwxxfa5vx86cc6kbn1ci4pwg03-min-nix-env-2.4pre20210802_47e96bb
+
+log() {
+    echo "$0:" "$@" >&2
+}
+
+die() {
+    log "FAILURE: $1"
+    exit 1
+}
+
+require_util() {
+    type "$1" > /dev/null 2>&1 || command -v "$1" > /dev/null 2>&1 || \
+        die "This script requires '$1', which could not be found."
+}
+
+check_dest() {
+    if [ -z "$UID" ]; then
+        die "\$UID is not set."
+    fi
+
+    log "Checking for empty '$dest' with uid='$UID' mode='0755'..."
+
+    if [ ! -e "$dest" ]; then
+        log "'$dest' does not exist. Please create it (e.g. with 'sudo install -d -m 0755 -o $UID $dest') and re-run this script."
+        exit 1
+    fi
+
+    dest_mode_and_uid="$(stat -c "%f %u" "$dest")"
+    if [ "$dest_mode_and_uid" != "41ed $UID" ]; then # 040755
+        die "'$dest' is not a directory with uid='$UID' and mode='0755'."
+    fi
+    if [ -n "$(ls "$dest")" ]; then
+        die "'$dest' is not empty."
+    fi
+}
+
+check_dest
+
+require_util date
+require_util curl
+require_util sha256sum
+require_util tar
+
+tarball_path="${TMPDIR:-/tmp}/$archive_name-$(date +%s).tar.gz"
+log "Fetching '$tarball_url' to '$tarball_path'..."
+trap "{ rm -f $tarball_path; }" EXIT
+curl -fL -o "$tarball_path" "$tarball_url"
+
+log "Verifying the integrity of '$tarball_path'..."
+observed_sha256="$(sha256sum "$tarball_path" | cut -c 1-64)"
+if [ "$tarball_sha256" != "$observed_sha256" ]; then
+    die "SHA-256 hash mismatch for '$tarball_path': expected $tarball_sha256, got $observed_sha256."
+fi
+
+log "Unpacking '$tarball_path' into '$dest'..."
+tar -xz -f $tarball_path -C $dest --strip 1 --delay-directory-restore
+chmod u+w $dest/store
+
+log "Initializing Nix database..."
+$env_store_path/bin/nix-store --init
+$env_store_path/bin/nix-store --load-db < $dest/reginfo
+rm -f $dest/reginfo
+
+log "Establishing Nix environment..."
+$env_store_path/bin/nix-store --realise --add-root $dest/env --indirect $env_store_path > /dev/null
+
+log
+log "SUCCESS"
+log
+log "To uninstall, simply remove '/nix'."
+log "You may want to create '/etc/nix/nix.conf'."
+log "Add the following to your shell init:"
+log
+log "   export PATH=\"$dest/env/bin:\$PATH\""
+log "   export MANPATH=\"$dest/env/share/man:\$MANPATH\""
+log "   export NIX_SSL_CERT_FILE=$dest/env/etc/ssl/certs/ca-bundle.crt # or your favorite cert bundle"
+log
+
+}
--- release.nix
+++ release.nix
@@ -1,67 +1,88 @@
-{ pkgs ? import ./nixpkgs {} }:
-
 assert builtins.storeDir == "/nix/store";
 
 let
 
-  inherit (pkgs) lib nixUnstable cacert buildEnv runCommand writeText linkFarm;
+  mk = { pkgs, tag }:
 
-  nix = nixUnstable;
+    let
+      inherit (pkgs) hostPlatform nixUnstable cacert buildEnv runCommand;
 
-  env = buildEnv {
-    name = "min-nix-env-${nix.version}";
-    paths = [ nix cacert ];
+    in rec {
+
+      nix = nixUnstable;
+
+      env = buildEnv {
+        name = "min-nix-env-${nix.version}";
+        paths = [ nix cacert ];
+      };
+
+      closure = pkgs.closureInfo { rootPaths = [ env ]; };
+
+      archiveName = "min-nix-${nix.version}-${hostPlatform.system}";
+
+      tarball = runCommand tarballName {} ''
+        dir=${archiveName}
+        reginfo=${closure}/registration
+
+        tar -czv -f $out \
+          --owner=0 --group=0 \
+          --sort=name \
+          --absolute-names \
+          --hard-dereference \
+          --transform "s,$reginfo,$dir/reginfo," \
+          --transform "s,$NIX_STORE,$dir/store,S" \
+          $reginfo $(cat ${closure}/store-paths)
+      '';
+
+      tarballName = "${archiveName}.tar.gz";
+
+      tarballUrl = "https://github.com/nspin/minimally-invasive-nix-installer/raw/${tag}/dist/${tarballName}";
+
+      scriptTemplate = runCommand scriptName {} ''
+        substitute ${./install.sh.in} $out \
+          --subst-var-by tarball_sha256 "$(sha256sum ${tarball} | cut -d ' ' -f 1)" \
+          --subst-var-by archive_name ${archiveName} \
+          --subst-var-by env_store_path ${env}
+      '';
+
+      script = runCommand scriptName {} ''
+        substitute ${scriptTemplate} $out \
+          --subst-var-by tarball_url ${tarballUrl}
+      '';
+
+      scriptName = "install-${hostPlatform.system}.sh";
+
+    };
+
+in
+
+let
+
+  pkgs = import ./nixpkgs {};
+
+  inherit (pkgs) lib writeText linkFarm;
+
+  platforms = lib.mapAttrs (_: pkgs: mk { inherit pkgs tag; }) {
+    x86_64 = pkgs;
+    aarch64 = pkgs.pkgsCross.aarch64-multiplatform;
   };
 
-  closure = pkgs.closureInfo { rootPaths = [ env ]; };
+  representativeContent = writeText "for-hash" (
+    toString (lib.mapAttrsToList (_: installer: installer.scriptTemplate) platforms)
+  );
 
-  archiveName = "min-nix-${nix.version}";
+  representativeHash = lib.substring 0 10
+    (lib.removePrefix "${builtins.storeDir}/" representativeContent.outPath);
 
-  tarball = runCommand tarballName {} ''
-    dir=${archiveName}
-    reginfo=${closure}/registration
+  tag = "dist-${representativeHash}";
 
-    tar -czv -f $out \
-      --owner=0 --group=0 \
-      --sort=name \
-      --absolute-names \
-      --hard-dereference \
-      --transform "s,$reginfo,$dir/reginfo," \
-      --transform "s,$NIX_STORE,$dir/store,S" \
-      $reginfo $(cat ${closure}/store-paths)
-  '';
-
-  scriptTemplate = runCommand scriptName {} ''
-    substitute ${./install.sh.in} $out \
-      --subst-var-by tarball_sha256 "$(sha256sum ${tarball} | cut -d ' ' -f 1)" \
-      --subst-var-by archive_name ${archiveName} \
-      --subst-var-by env_store_path ${env}
-  '';
-
-  mkScript = { tarballUrl }: runCommand scriptName {} ''
-    substitute ${scriptTemplate} $out \
-      --subst-var-by tarball_url ${tarballUrl}
-  '';
-
-  scriptName = "install.sh";
-  tarballName = "${archiveName}.tar.gz";
-
-  tag = "dist-${lib.substring 0 10 (lib.removePrefix "${builtins.storeDir}/" scriptTemplate.outPath)}";
-  tarballUrl = "https://github.com/nspin/minimally-invasive-nix-installer/raw/${tag}/dist/${tarballName}";
-
-  script = mkScript {
-    inherit tarballUrl;
-  };
-
-  links = linkFarm "links" [
-    { name = tarballName; path = tarball; }
-    { name = scriptName; path = script; }
+  links = linkFarm "links" ([
     { name = "TAG"; path = writeText "TAG" tag; }
-  ];
+  ] ++ lib.concatLists ((lib.flip lib.mapAttrsToList platforms (_: installer: with installer; [
+    { name = scriptName; path = script; }
+    { name = tarballName; path = tarball; }
+  ]))));
 
-in rec {
-
-  inherit nix env tarball script links;
-  inherit mkScript;
-
-}
+in {
+  inherit links pkgs;
+} // platforms
